427f0a01153977b21e33b28df3503bf28c0817a5
nevilbhimani <bhimaninevil72@gmail.com>
2021-12-13 01:52:14 +0530

    styling added to input fields,formik is used to input value and submit

--- src/components/loginWith.js
+++ src/components/loginWith.js
@@ -3,8 +3,19 @@ import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import "../styles/loginWith.css";
+import { useFormik } from "formik";
 import { BrowserRouter as Router, Link, Route } from "react-router-dom";
 const LoginWith = () => {
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
+  console.log(formik.values);
   return (
     <div className="loginDiv">
       <Grid container>
@@ -14,20 +25,41 @@ const LoginWith = () => {
         <Grid item xs={12}>
           <Typography className="textInGreen size-150">Email</Typography>
         </Grid>
+
         <Grid item xs={12}>
-          <form className="styledForm" />
+          <input
+            className="styledInput"
+            id="email"
+            name="email"
+            type="email"
+            onChange={formik.handleChange}
+            value={formik.values.email}
+          />
         </Grid>
         <Grid item xs={12}>
           <Typography className="textInGreen size-150">Password</Typography>
         </Grid>
         <Grid item xs={12}>
-          <form className="styledForm" />
+          <input
+            className="styledInput"
+            id="password"
+            name="password"
+            type="password"
+            onChange={formik.handleChange}
+            value={formik.values.password}
+          />
         </Grid>
         <Grid item xs={12}>
-          <Button variant="contained" className="styledButtonForL">
+          <Button
+            type="submit"
+            variant="contained"
+            className="styledButtonForL"
+            onClick={formik.handleSubmit}
+          >
             Login
           </Button>
         </Grid>
+
         <Grid item xs={8}>
           <Typography className="newUser">New User?</Typography>
         </Grid>
--- src/components/register.js
+++ src/components/register.js
@@ -1,10 +1,22 @@
 import React from "react";
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
+import { useFormik } from "formik";
 import Button from "@mui/material/Button";
 import "../styles/loginWith.css";
 import "../styles/register.css";
 const Register = () => {
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      setPassword: "",
+      confirmPassword: "",
+    },
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
   return (
     <>
       <div className="loginDiv">
@@ -19,30 +31,60 @@ const Register = () => {
             <Typography className="textInGreen ">Name</Typography>
           </Grid>
           <Grid item xs={12}>
-            <form className="styledForm height-20 width-80" />
+            <input
+              className="styledInput height-20 width-80"
+              id="namw"
+              name="name"
+              type="name"
+              onChange={formik.handleChange}
+              value={formik.values.name}
+            />
           </Grid>
           <Grid item xs={12}>
             <Typography className="textInGreen ">Email</Typography>
           </Grid>
           <Grid item xs={12}>
-            <form className="styledForm height-20 width-80" />
+            <input
+              className="styledInput height-20 width-80"
+              id="email"
+              name="email"
+              type="email"
+              onChange={formik.handleChange}
+              value={formik.values.email}
+            />
           </Grid>
           <Grid item xs={12}>
             <Typography className="textInGreen">Set Password</Typography>
           </Grid>
           <Grid item xs={12}>
-            <form className="styledForm height-20 width-80" />
+            <input
+              className="styledInput height-20 width-80"
+              id="setPassword"
+              name="setPassword"
+              type="setPassword"
+              onChange={formik.handleChange}
+              value={formik.values.setPassword}
+            />
           </Grid>
           <Grid item xs={12}>
             <Typography className="textInGreen">Confirm Password</Typography>
           </Grid>
           <Grid item xs={12}>
-            <form className="styledForm height-20 width-80" />
+            <input
+              className="styledInput height-20 width-80"
+              id="ConfirmPassword"
+              name="ConfirmPassword"
+              type="ConfirmPassword"
+              onChange={formik.handleChange}
+              value={formik.values.ConfirmPassword}
+            />
           </Grid>
           <Grid item xs={12}>
             <Button
               variant="contained"
               className="styledButtonForL width-80 marginR"
+              type="submit"
+              onClick={formik.handleSubmit}
             >
               Login
             </Button>
